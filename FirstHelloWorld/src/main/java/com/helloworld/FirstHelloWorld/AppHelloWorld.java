9795ff99ad32336a8f68621b31421e449ef6499e
awadahazman <45381772+awadahazman@users.noreply.github.com>
2018-11-26 22:46:03 -0800

    Add files via upload

---
+++ FirstHelloWorld/src/main/java/com/helloworld/FirstHelloWorld/AppHelloWorld.java
@@ -0,0 +1,10 @@
+package com.helloworld.FirstHelloWorld;
+
+public class AppHelloWorld {
+	
+	 public static void main( String[] args )
+	    {
+	        System.out.println("Hello World!");
+	    }
+
+}
